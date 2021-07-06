61879de992a9a9b0d21aebe9f5cf118221fac34c
Amine Chograni <aminechograni@gmail.com>
2021-07-06 15:20:45 +0100

    gg

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
 @section('content')
-
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->content}}</p>
+    <em>{{$post->created_at}}</em>
+    
+    <span>{{$post->active}}</span>
 @endsection
